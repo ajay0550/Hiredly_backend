f2d897423feaf48bcc66bd4b29944223a5f0f3bd
ajay0550 <ajaygundu03@gmail.com>
2026-02-20 09:51:08 +0530

    feat: initialize backend with MVC structure

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("HireHub Backend Running 🚀");
+});
+
+module.exports = app;
